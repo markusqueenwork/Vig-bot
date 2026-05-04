960519a733afe2eb39a6b8ce40ba18dd4c701ccd
markusqueenwork <markusqueenwork@gmail.com>
2026-05-04 15:41:58 +0300

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -11,7 +11,7 @@ BACKEND_URL = "https://voiceinside-backend.onrender.com"
 
 TARIFFS = {
     "1_month": {"name": "1 месяц", "price": 300, "days": 30},
-    "3_months": {"name": "3 месяца", "price": 700, "days": 90},
+    "3_months": {"name": "3 месяца", "price": 1, "days": 90},
     "6_months": {"name": "6 месяцев", "price": 1500, "days": 180},
     "1_year": {"name": "1 год", "price": 3500, "days": 365}
 }
